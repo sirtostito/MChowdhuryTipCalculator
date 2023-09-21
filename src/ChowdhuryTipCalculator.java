52bb1e8c288152a302fc6250497dc79de02f610c
BT_2N2_07 <sample@bths.edu>
2023-09-21 13:33:54 -0400

    commit 3: Minor debugging + Started receipt output

--- src/ChowdhuryTipCalculator.java
+++ src/ChowdhuryTipCalculator.java
@@ -15,16 +15,27 @@ public class ChowdhuryTipCalculator {
         System.out.println("How many dishes have you ordered off our menu today?");
         int dishes = scan.nextInt();
         int num = dishes;
-        double tips;
-        double totalTips= 0;
+        double cost;
+        double totalCost= 0;
 
         while (num > 0) {
             System.out.print("How much did each of your dishes cost?");
-            tips = scan.nextDouble();
+            cost = scan.nextDouble();
             scan.nextLine();
-            totalTips += tips;
+            totalCost += cost;
             num -= 1;
         }
+
+        double tips = totalCost/tip;
+
+        System.out.println("RECEIPT: ");
+        System.out.println("Party of: " + party);
+        System.out.println("Dishes ordered: " + dishes);
+        System.out.println("====================================");
+        System.out.println("Total bill before tip: $" + totalCost);
+        System.out.println("Percentage tipped: " + tip + "%");
+        System.out.println("Total tip: $" + tips);
+
 
     }
 }
